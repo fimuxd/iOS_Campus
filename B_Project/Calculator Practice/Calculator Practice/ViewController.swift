ae7b3fca52e4b36839e92cea85e403285f53a98e
Bo-Young PARK <iOSDev@Bo-Youngui-MacBook-Pro.local>
2017-05-10 17:48:52 +0900

    2nd commit

---
+++ B_Project/Calculator Practice/Calculator Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator Practice
+//
+//  Created by Bo-Young PARK on 10/5/2017.
+//  Copyright © 2017 Bo-Young Park, Byron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
