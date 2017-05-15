d934e98130b095f45e419fd039759c00592920b4
Bo-Young PARK <iOSDev@Bo-Youngui-MacBook-Pro.local>
2017-05-15 22:01:51 +0900

    update func playground-self study

--- A_LectureSummary/170512/MyPlayground.playground/Contents.swift
+++ A_LectureSummary/170512/MyPlayground.playground/Contents.swift
@@ -49,7 +49,7 @@ print(evenNumber1)
 
 print("\n")
 
-//2. else는 보안써도 무방해서 지울 수 있다. 하지만 거짓일 경우의 조건을 입력하지 않은 것이므로, 거짓일 경우에는 return이 되지 않는다. 따라서 if문 바깥, func의 내부에 return false를 별도 입력해주어야 한다.
+//2. else는 안써도 무방해서 지울 수 있다. 하지만 거짓일 경우의 조건을 입력하지 않은 것이므로, 거짓일 경우에는 return이 되지 않는다. 따라서 if문 바깥, func의 내부에 return false를 별도 입력해주어야 한다.
 
 func isEven2(number num:Int) -> Bool {
     if (num % 2 ) == 0 {
@@ -66,40 +66,73 @@ print("\n")
 
 //3.  튜플과 조건문, 연산자를 이용한 함수 작성
 
-let h:(floor:Int, location:String, money:Int) = (12, "Seoul", 5000)
+/* 아래는 수업시간에 해본 예제
+ 
+ let h:(floor:Int, location:String, money:Int) = (12, "Seoul", 5000)
+ 
+ func isAble(myHome: (floor:Int, location:String, money:Int)) -> Bool
+ {
+ 
+ if myHome.floor >= 10 && myHome.location == "Seoul" && myHome.money <= 8000
+ {
+ print("완벽한 나의 집")
+ return true
+ }
+ else if (myHome.0 >= 10 && myHome.1 == "Seoul") || myHome.2 <= 8000
+ {
+ print("괜찮은 나의 집")
+ return true
+ }
+ else if myHome.floor >= 10 || myHome.location == "Seoul" || myHome.money <= 8000
+ {
+ print("좀 더 알아보자")
+ return true
+ }
+ 
+ return false
+ }
+ 
+ print(isAble(myHome: (h)))
+ 
+ let h1:(floor:Int, location:String, money:Int) = (9, "Gyeonggi-do", 3000)
+ 
+ print(isAble(myHome: (h1)))
+ */
 
-func isAble(myHome: (floor:Int, location:String, money:Int)) -> Bool
+// 내가 스스로 해본 복습
+
+let myCreditCard:(type:String, limit:UInt, color:String) = ("Visa", 500, "Silver")
+func isAble(creditCard:(type:String, limit:UInt, color:String)) -> Bool
 {
-    
-    if myHome.floor >= 10 && myHome.location == "Seoul" && myHome.money <= 8000
+    if creditCard.0 == "Visa" && creditCard.1 >= 500 && creditCard.2 == "Silver"
     {
-        print("완벽한 나의 집")
+        print("최고의 신용카드")
         return true
     }
-    else if (myHome.0 >= 10 && myHome.1 == "Seoul") || myHome.2 <= 8000
+    else if creditCard.0 == "Visa" && creditCard.1 >= 500 || creditCard.2 == "Silver"
     {
-        print("괜찮은 나의 집")
+        print("괜찮은 신용카드")
         return true
     }
-    else if myHome.floor >= 10 || myHome.location == "Seoul" || myHome.money <= 8000
+    else if creditCard.0 == "Visa" || creditCard.1 >= 500 || creditCard.2 == "Silver"
     {
-        print("좀 더 알아보자")
+        print("그냥 신용카드")
         return true
     }
-
-    return false
+    else
+    {
+        print("다른거 알아봐")
+        return false
+    }
 }
 
-print(isAble(myHome: (h)))
+isAble(creditCard: myCreditCard)
 
-let h1:(floor:Int, location:String, money:Int) = (9, "Gyeonggi-do", 3000)
-
-print(isAble(myHome: (h1)))
 
 print("\n")
 
 //4. Swap 을 이용한 함수 작성
-
+/* 수업시간 해본 예제
 func swapToInt(_ num1:inout Int, _ num2:inout Int)
 {
     let temp:Int = num1
@@ -123,11 +156,33 @@ func minus(firstNum num1:Int, secondNum num2:Int) -> Int
 
 print(minus(firstNum: 5, secondNum: 10))
 print(minus(firstNum: 10, secondNum: 5))
+*/
+
+func swapToInt(_ num1:inout Int, _ num2:inout Int)
+{
+    let temp:Int = num1
+    num1 = num2
+    num2 = temp
+}
+
+func minus(num1:Int, num2:Int) -> Int
+{
+    var vNum1:Int = num1
+    var vNum2:Int = num2
+    
+    if vNum1 > vNum2
+    {
+        swapToInt(&vNum1, &vNum2)
+    }
+    return vNum1 - vNum2
+}
+
+minus(num1: 4, num2: 5)
 
 print("\n")
 
 //5. 시험점수에 따라 학점을 배분하는 코드
-
+/*
 func gradeCal(score:Double) -> String
 {
     if(100 >= score && 95 < score)
@@ -177,10 +232,22 @@ return "올바른 점수를 입력해주세요."
 }
 
 gradeCal(score: 14)
+*/
+
+func scoreToGrade(number score:UInt) -> String
+{
+    var grade:String = "F"
+    
+    if score > 95 && score <= 100
+    {
+        print("A+")
+    }
+}
 
 print("\n")
 
 //6. Grade를 받아서 Point로 변환해주는 함수 (예. Point=4.5, 4.0, 3.5 등)
+/*
 func pointCal(grade: String)
 {
     if(grade == "A+")
@@ -228,10 +295,14 @@ func pointCal(grade: String)
 pointCal(grade: "A+")
 pointCal(grade: "good")
 
+*/
+
 print("\n")
 
 //7. 점수를 포인터로 변환하기 > function으로 다시할 것
+/*
 pointCal(grade: gradeCal(score: 92))
+*/
 
 print("\n")
 
