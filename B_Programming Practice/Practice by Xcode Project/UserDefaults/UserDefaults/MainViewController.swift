79185574fb893504adc635a56261ab9d4948e84e
fimuxd@gmail.com <fimuxd@gmail.com>
2017-05-30 22:06:16 +0900

    practice UserDefault

--- B_Programming Practice/Practice by Xcode Project/UserDefaults/UserDefaults/MainViewController.swift
+++ B_Programming Practice/Practice by Xcode Project/UserDefaults/UserDefaults/MainViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    
+    
     @IBOutlet weak var segueOnOff: UISegmentedControl!
+    
+    
+    
+    
+    /***********************************************************************************/
+    //                      자습_UserDefault & Present Modally 활용                       //
+    /***********************************************************************************/
+
+    //VC에서 받아온 값을 표시해줄 Label
+    @IBOutlet weak var wecomeLabel: UILabel!
+    
+    //ViewController에서 Segua로 넘긴 data값(UserDefault로 입력했던 값)을 받아올 변수 설정
+    var data1:String?
+    var data2:String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        if let lastName = data1, let firstName = data2 {
+        
+        wecomeLabel.text = "\(lastName)" + "\(firstName)" + "님, 안녕하세요"
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         if segueOnOff.selectedSegmentIndex == 0 {
@@ -30,15 +48,18 @@ class MainViewController: UIViewController {
             return false
         }
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let secondVC:SecondViewController = segue.destination as! SecondViewController
-        secondVC.data = "Bo-Young"
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
+    
     
     @IBAction func unwindSegue(unwindSegue:UIStoryboardSegue) {
         
     }
+    
+    
     
 
     /*
--- B_Programming Practice/Practice by Xcode Project/UserDefaults/UserDefaults/ViewController.swift
+++ B_Programming Practice/Practice by Xcode Project/UserDefaults/UserDefaults/ViewController.swift
@@ -8,29 +8,102 @@
 
 import UIKit
 
-let ageKey = "AGE"
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var nameResultLabel: UILabel!
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    /***********************************************************************************/
+    //                      수업시간에 해본 간단한 예제_UserDefault 이해하기                     //
+    /***********************************************************************************/
+    
+    // @IBOutlet weak var nameResultLabel: UILabel!
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //
+    //        UserDefaults.standard.set("Bo-Young", forKey: "First Name")
+    //        UserDefaults.standard.set("Park", forKey: "Last Name")
+    //        UserDefaults.standard.set(31, forKey: "Age")
+    
+    //        let firstName:String = UserDefaults.standard.string(forKey: "First Name")!
+    //        let lastName:String = UserDefaults.standard.string(forKey: "Last Name")!
+    //        let age:Int = UserDefaults.standard.object(forKey: "Age") as! Int
+    //
+    //        nameResultLabel.text = "이름 : \(lastName)" + "\( firstName)" + ". 나이는 \(age) 입니다."
+    //     }
+    
+    
+    
+    /***********************************************************************************/
+    //          자습_UITextFieldDelegate & UserDefault & Present Modally 활용             //
+    /***********************************************************************************/
+    
+    
+    // 성, 이름, 나이에 대한 TextField 연결
+    @IBOutlet weak var lNTextField: UITextField!
+    @IBOutlet weak var fNTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TextField에서 Return Key 눌렀을 때, 다음 TF로 넘어가다가 마지막에는 키보드가 내려가는 method 구현을 위한 Delegate 설정
+        lNTextField.delegate = self
+        fNTextField.delegate = self
+        ageTextField.delegate = self
+        
+    }
     
-//        UserDefaults.standard.set("Bo-Young", forKey: "First Name")
-//        UserDefaults.standard.set("Park", forKey: "Last Name")
-//        UserDefaults.standard.set(31, forKey: "Age")
-        
-        let firstName:String = UserDefaults.standard.string(forKey: "First Name")!
+    // TextField에서 Return Key 눌렀을 때, 다음 TF로 넘어가다가 마지막에는 키보드가 내려가는 method 구현
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 1 {
+            fNTextField.becomeFirstResponder()
+        }else if textField.tag == 2 {
+            ageTextField.becomeFirstResponder()
+        }else {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    
+    // '입력' 버튼을 눌렀을 때, TextField 상에 입력한 각각의 내용들이 UserDefault 상에 저장되도록 method 설정
+    @IBAction func inputBtn(_ sender: UIButton) {
+
+            //1. UserDefault Set을 먼저 해주어야 한다
+            UserDefaults.standard.set(lNTextField.text, forKey: "Last Name")
+            UserDefaults.standard.set(fNTextField.text, forKey: "First Name")
+            UserDefaults.standard.set(ageTextField.text, forKey: "Age")
+            
+        //2. Set된 UserDefualt의 Key값에 대한 변수 설정
         let lastName:String = UserDefaults.standard.string(forKey: "Last Name")!
-        let age:Int = UserDefaults.standard.object(forKey: "Age") as! Int
-  
-        nameResultLabel.text = "이름 : \(lastName)" + "\( firstName)" + ". 나이는 \(age) 입니다."
+        let firstName:String = UserDefaults.standard.string(forKey: "First Name")!
+        let age:String = UserDefaults.standard.string(forKey: "Age")!
+
         
+        //3. 제대로 저장되었는지 확인용ㅋㅋ
+            print(lastName)
+            print(firstName)
+            print(age)
+    
     
     }
-
+    
+    
+    // UserDefault에 저장된 값 Data를 MainVC로 Segua할 때, 넘겨준 다음 MainVC에 있는 Label에 표시되도록 하려고 함
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let mainVC:MainViewController = segue.destination as! MainViewController
+        mainVC.data1 = lNTextField.text
+        mainVC.data2 = fNTextField.text
+        
+    }
+    
+    
+    //Segue Rewind(Dismiss)
+    @IBAction func rewindSegue(backToVC:UIStoryboardSegue) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
