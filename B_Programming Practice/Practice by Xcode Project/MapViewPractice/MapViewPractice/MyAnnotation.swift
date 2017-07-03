ff93b6a327cac48e9a04d0d38d39c166ba7d7025
FIMUXD <fimuxd@gmail.com>
2017-07-03 10:16:47 +0900

    update Mapview

---
+++ B_Programming Practice/Practice by Xcode Project/MapViewPractice/MapViewPractice/MyAnnotation.swift
@@ -0,0 +1,23 @@
+//
+//  MyAnnotation.swift
+//  MapViewPractice
+//
+//  Created by Bo-Young PARK on 30/6/2017.
+//  Copyright © 2017 Bo-Young PARK. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MyAnnotation: NSObject, MKAnnotation {
+
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate:CLLocationCoordinate2D, title:String, subtitle:String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
